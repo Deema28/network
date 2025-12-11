d6a330c9b7fd4afc145abc16bbf194de50b146f4
Tala1i <tala75799@gmail.com>
2025-12-11 04:17:32 +0300

    edit

--- Server.py
+++ Server.py
@@ -1,48 +1,74 @@
 import socket
+import threading
 
 HOST = "0.0.0.0"
 PORT = 5000
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 server_socket.bind((HOST, PORT))
-
 server_socket.listen()
 
+active_clients = 0
+clients_lock = threading.Lock()
+
+
+def handle_client(conn, addr):
+    global active_clients
+
+    with clients_lock:
+        active_clients += 1
+        print(f"[INFO] New client {addr[0]}:{addr[1]} | Active clients: {active_clients}")
+
+    try:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                print(f"[*] Client {addr[0]} closed the connection.")
+                break
+
+            message = data.decode().strip()
+            print(f"From[{addr[0]}] Received: {message}")
+
+           
+            if message.lower() == "finish":
+                print(f"[*] Client {addr[0]} requested to close the connection with FINISH.")
+                break
+
+            try:
+                base_str, exp_str = message.split("^")
+
+                base = int(base_str)
+                exponent = int(exp_str)
+
+                result = base ** exponent
+                response = f"{base}^{exponent} = {result}"
+
+            except Exception:
+                response = "ERROR: send as: base^exponent (e.g., 3^5)"
+
+            conn.sendall(response.encode())
+
+    finally:
+        conn.close()
+        with clients_lock:
+            active_clients -= 1
+            print(f"[*] Connection with {addr[0]} closed. Active clients: {active_clients}")
+
+
 def main():
+    print(f"[SERVER] Listening on {HOST}:{PORT} ...")
 
- while True :
-   print("[SERVER] Waiting for a client...")
+    while True:
+        print("[SERVER] Waiting for a client...")
+        conn, addr = server_socket.accept()
+        print(f"[+] Client connected from {addr}")
 
-   conn, addr = server_socket.accept()
-   print(f"[+] Client connected from {addr}")
-
-   while True:
-       data = conn.recv(1024).decode().strip()
-  
-       if not data:
-           print("[*] Client closed the connection.")
-           break
-
-       
-       print("Received:", data)
-
-       if data.lower() == "quit":
-           print("[*] Client requested to close the connection.")
-           break
-       try:
-           base_str, exp_str = data.split("-")   
-           base =  float(base_str)             
-           exponent = float(exp_str)          
-
-           result = base ** exponent          
-           response = str(result)         
-       except Exception:
-           response = "ERROR: send as: base-exponent"
-
-       conn.sendall(response.encode())
-   conn.close()
-   print("[*] Connection closed.")
+        client_thread = threading.Thread(
+            target=handle_client,
+            args=(conn, addr),
+            daemon=True
+        )
+        client_thread.start()
 
 
 if __name__ == "__main__":
