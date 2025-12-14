6d42975e141afb152c5a3c7fd485ff02fac992a0
Deema28 <deemafaisal28@gmail.com>
2025-12-14 04:38:46 +0300

    add comment

--- Client.py
+++ Client.py
@@ -2,44 +2,51 @@ import socket
 import time
 
 
-
-HOST = "172.20.10.2"
-PORT = 5000
+# Server Configuration
+HOST = "172.20.10.2"  # Server IP address
+PORT = 5000 # Port number match the Server for the TCP connection
 
 def main():
+    # Create a TCP socket using IPv4 and TCP protocol
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    # Connect to the server
     client_socket.connect((HOST, PORT))
     print(f"[CLIENT] Connected to server {HOST}:{PORT}")
 
     while True:
-        
+        # Take user input in the format(base^exponent)
         user_input = input("Enter expression (format: base^exponent), or type 'Finish' to exit: ").strip()
 
+        # Handle empty input
         if not user_input:
             continue
 
-        
+        # If user wants to terminate the connection
         if user_input.lower() == "finish":
             client_socket.sendall(user_input.encode()) 
             print("[CLIENT] Closing connection...")
             break
-
-      
+    
+        # Record start time before sending the request
         start_time = time.time()
 
-        
+        # Send the input data to the server
         client_socket.sendall(user_input.encode())
 
-        
+        # Receive response from the server
         response = client_socket.recv(1024).decode().strip()
 
-        
+        # Record end time after receiving the response
         end_time = time.time()
-        rtt_ms = (end_time - start_time) * 1000
 
+        # Calculate Round-Trip Time (RTT) in milliseconds
+        rtt_ms = (end_time - start_time) * 1000
+    
+        # Display the result and RTT
         print(f"Result from server: {response}  |  RTT: {rtt_ms:.3f} ms\n")
 
+    # Close the socket connection
     client_socket.close()
     print("[CLIENT] Connection closed.")
 
--- Server.py
+++ Server.py
@@ -1,13 +1,19 @@
 import socket
 import threading
 
-HOST = "0.0.0.0"
-PORT = 5000
+HOST = "0.0.0.0" # Listen on all available network interfaces
+PORT = 5000  # Port number match the clint for the TCP connection
 
+# Create a TCP socket using IPv4 and TCP protocol
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the IP address and port number
 server_socket.bind((HOST, PORT))
+
+# Start listening for incoming connections
 server_socket.listen()
 
+# variables to track active clients safely
 active_clients = 0
 clients_lock = threading.Lock()
 
@@ -15,40 +21,50 @@ clients_lock = threading.Lock()
 def handle_client(conn, addr):
     global active_clients
 
+    # Increment active client count safely using a lock
     with clients_lock:
         active_clients += 1
         print(f" • New client {addr[0]}:{addr[1]} | Active clients: {active_clients}\n")
 
     try:
         while True:
+            # Receive data from the client
             data = conn.recv(1024)
+
+            # If no data is received, the client disconnected
             if not data:
                 print(f" • Client {addr} closed the connection.")
                 break
-
+            # Decode the message
             message = data.decode().strip()
             print(f" From[{addr}] Received: {message}\n")
 
-           
+            # If client requests to terminate the connection
             if message.lower() == "finish":
                 print(f" • Client {addr} requested to close the connection with FINISH.")
                 break
 
             try:
+                # Split the received message into base and exponent
                 base_str, exp_str = message.split("^")
 
+                # Convert input values to integers
                 base = int(base_str)
                 exponent = int(exp_str)
 
+                # Prepare the response
                 result = base ** exponent
                 response = f"{base}^{exponent} = {result}"
 
             except Exception:
+                # Handle invalid input format
                 response = " x ERROR: send as: base^exponent (e.g., 3^5)"
 
+            # Send the response back to the client
             conn.sendall(response.encode())
 
     finally:
+        # Close the connection and update client count
         conn.close()
         with clients_lock:
             active_clients -= 1
@@ -60,9 +76,11 @@ def main():
 
     while True:
         print(" [SERVER] Waiting for a client...\n")
+        # Accept a new client connection
         conn, addr = server_socket.accept()
         print(f" + Client connected from {addr}\n")
 
+        # Create a new thread for each connected client
         client_thread = threading.Thread(
             target=handle_client,
             args=(conn, addr),
